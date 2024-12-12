9e2bb15062db4b7accd003a867ec65bea6dc0f55
Trong Nguyen <Trong Nguyen@STYLE-OF-ARIES>
2024-12-12 20:17:40 +0700

    Add README.md.

---
+++ Test/Sach.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Sach
+    {
+        #region Thuoc tinh
+        string maSach, tenSach, tenTacGia;
+        int soTrang, gia;
+        #endregion
+        #region Phuong thuc
+        public string getMaSach()
+        {
+            return maSach;
+        }
+        public void input()
+        {
+            Console.Write("Ma sach: ");
+            maSach = Console.ReadLine();
+            Console.Write("Ten sach: ");
+            tenSach = Console.ReadLine();
+            Console.Write("Ten tac gia: ");
+            tenTacGia = Console.ReadLine();
+            Console.Write("So trang: ");
+            soTrang = Int32.Parse(Console.ReadLine());
+            Console.Write("Gia: ");
+            gia = Int32.Parse(Console.ReadLine());
+        }
+        public void output()
+        {
+            Console.WriteLine("Ma sach: {0}; Ten sach: {1}; Ten tac gia: {2}; So trang: {3}; Gia sach(VND): {4}", maSach, tenSach, tenTacGia, soTrang, gia);
+        }
+        #endregion
+    }
+}
---
+++ Test/SinhVien.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class SInhVien
+    {
+        public String maSV { get; set; }
+        public String hoTen { get; set; }
+        public void input()
+        {
+            Console.Write("Ma Sinh Vien: ");
+            maSV = Console.ReadLine();
+            Console.Write("Ho ten: ");
+            hoTen = Console.ReadLine();
+        }
+        public void output()
+        {
+            Console.WriteLine("Ma Sinh Vien: {0} \t Ho va ten: {1}", maSV, hoTen);
+        }
+    }
+}
